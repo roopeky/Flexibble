fa924c9a59d22f97865eecd0649ee16df71c7c1c
roopeky <roope.kylli@metropolia.fi>
2023-10-16 14:31:30 +0300

    footer progress

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,25 @@
 import React from "react";
 import Image from "next/image";
+import { footerLinks } from "@/constants";
+import Link from "next/link";
+
+type ColumnProps = {
+  title: string;
+  links: Array<string>;
+};
+
+const FooterColumn = ({ title, links }: ColumnProps) => {
+  <div className="footer_column">
+    <h4 className="font-semibold ">{title}</h4>
+    <ul className="flex flex-col gap-2 font-normal">
+      {links.map((link) => (
+        <Link href="/" key={link}>
+          {link}
+        </Link>
+      ))}
+    </ul>
+  </div>;
+};
 
 const Footer = () => {
   return (
@@ -10,7 +30,7 @@ const Footer = () => {
             src="/logo-purple.svg"
             width={115}
             height={38}
-            alt="Flexibble"
+            alt="logo-purple"
           />
 
           <p className="text-start text-sm font-normal mt-5 max-w-xs">
@@ -18,9 +38,15 @@ const Footer = () => {
             work process, tips, and tricks.
           </p>
         </div>
+        <div className="flex flex-wrap gap-12">
+          <FooterColumn
+            title={footerLinks[0].title}
+            links={footerLinks[0].links}
+          />
+        </div>
       </div>
     </footer>
   );
 };
-
+// @ youtube 31:48
 export default Footer;
