011cf65f05d6d6844a8ff0b69deed4852f68f955
roopeky <roope.kylli@metropolia.fi>
2023-10-17 19:39:33 +0300

    footer finished

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import { footerLinks } from "@/constants";
 import Link from "next/link";
 
+const year = new Date().getFullYear();
+
 type ColumnProps = {
   title: string;
   links: Array<string>;
@@ -43,7 +45,46 @@ const Footer = () => {
             title={footerLinks[0].title}
             links={footerLinks[0].links}
           />
+
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterColumn
+              title={footerLinks[1].title}
+              links={footerLinks[1].links}
+            />
+            <FooterColumn
+              title={footerLinks[2].title}
+              links={footerLinks[2].links}
+            />
+          </div>
+
+          <FooterColumn
+            title={footerLinks[3].title}
+            links={footerLinks[3].links}
+          />
+
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterColumn
+              title={footerLinks[4].title}
+              links={footerLinks[4].links}
+            />
+            <FooterColumn
+              title={footerLinks[5].title}
+              links={footerLinks[5].links}
+            />
+          </div>
+          <FooterColumn
+            title={footerLinks[6].title}
+            links={footerLinks[6].links}
+          />
         </div>
+      </div>
+
+      <div className="flexBetween footer_copyright">
+        <p>@ {year} Flexibble. All rights reserved.</p>
+        <p className="text-gray">
+          <span className="text-black font-semibold">10,214</span> projects
+          submitted
+        </p>
       </div>
     </footer>
   );
