058c8c261808ca897dc8041a5ebb44fd1ec123ef
roopeky <roope.kylli@metropolia.fi>
2023-10-18 11:51:05 +0300

    profilemenu component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { NavLinks } from "@/constants";
 import AuthProviders from "./AuthProviders";
 import { getCurrentUser } from "@/lib/session";
+import ProfileMenu from "./ProfileMenu";
 
 const Navbar = async () => {
   const session = await getCurrentUser();
@@ -26,17 +27,7 @@ const Navbar = async () => {
       <div className="flexCenter gap-4">
         {session?.user ? (
           <>
-            {session?.user?.image && (
-              <Link href={`/profile/${session?.user?.id}`}>
-                <Image
-                  src={session.user.image}
-                  width={40}
-                  height={40}
-                  className="rounded-full"
-                  alt={session.user.name}
-                />
-              </Link>
-            )}
+            <ProfileMenu session={session} />
             <Link href="/create-project">Share Work</Link>
           </>
         ) : (
