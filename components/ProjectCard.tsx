fa6a73dec40c9522063fb31c5f839679c4a5684a
roopeky <roope.kylli@metropolia.fi>
2023-10-22 13:22:52 +0300

    under projects pic it shows creators profiel and also hovering on project pic shows the title of the project

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -25,7 +25,25 @@ const ProjectCard = ({ id, image, title, name, avatarUrl, userId }: Props) => {
           className="w-full h-full object-cover rounded-2xl"
           alt="Project image"
         />
+
+        <div className="hidden group-hover:flex profile_card-title">
+          <p className="w-full">{title}</p>
+        </div>
       </Link>
+
+      <div className="flexBetween w-full px-2 mt-3 font-semibold text-sm">
+        <Link href={`/profile/${userId}`}>
+          <div className="flexCenter gap-2">
+            <Image
+              src={avatarUrl}
+              width={24}
+              height={24}
+              className="rounded-full"
+              alt="profile picture"
+            />
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
