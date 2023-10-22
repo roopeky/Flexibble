b448c9752f96b1b25e6e3cd8b060d08e132098dd
roopeky <roope.kylli@metropolia.fi>
2023-10-22 15:24:10 +0300

    better text

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = async () => {
         {session?.user ? (
           <>
             <ProfileMenu session={session} />
-            <Link href="/create-project">Share Work</Link>
+            <Link href="/create-project">Share Project</Link>
           </>
         ) : (
           <AuthProviders />
